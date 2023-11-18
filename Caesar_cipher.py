3a559f849bf8f89011185b702fde31956d3de550
Naren Pradhan <narenpradhan05@gmail.com>
2023-11-18 10:48:25 +0530

    modify the interface of the cipher

--- Caesar_cipher.py
+++ Caesar_cipher.py
@@ -1,14 +1,31 @@
+import time
+
+
 letters = "abcdefghijklmnopqrstuvwxyz"
 numletters = len(letters)
 
-def encrypt_decrypt(user_input,key,mode):
+
+def banner():
+    print(
+"""\033[38;5;202m
+-----------------------------------------------------------------
+   ___                               ___ _       _               
+  / __\__ _  ___  ___  __ _ _ __    / __(_)_ __ | |__   ___ _ __ 
+ / /  / _` |/ _ \/ __|/ _` | '__|  / /  | | '_ \| '_ \ / _ \ '__|
+/ /__| (_| |  __/\__ \ (_| | |    / /___| | |_) | | | |  __/ |   
+\____/\__,_|\___||___/\__,_|_|    \____/|_| .__/|_| |_|\___|_|   
+                                          |_|                    
+-----------------------------------------------------------------""")
+
+
+def encrypt_decrypt(user_input, key, mode):
     result = ""
-    if mode == 'd':
+    if mode == "d":
         key = -key
-    
+
     for letter in user_input:
         letter = letter.lower()
-        if letter != ' ':
+        if letter != " ":
             index = letters.find(letter)
             if index == -1:
                 result += letter
@@ -21,14 +38,26 @@ def encrypt_decrypt(user_input,key,mode):
             pass
     return result
 
+
 # Driver Code
-mode = input("[*] Do you want to Encrypt or Decrypt (e\d) : ")
-key = int(input("[*] Enter the key : "))
-if mode == 'e':
-    user_input = input("[*] Enter text to Encrypt : ")
-    result = encrypt_decrypt(user_input,key,mode)
-    print(f'[+] Encrypted Text : {result}')
-elif mode == 'd':
-    user_input = input("Enter text to Decrypt : ")
-    result = encrypt_decrypt(user_input,key,mode)
-    print(f'[+] Decrypted Text : {result}')
+banner()
+
+mode = input("\n\033[38;5;230m[*] Do you want to Encrypt or Decrypt (e\d) : ")
+key = int(input("\033[38;5;230m[*] Enter the key : "))
+
+if mode == "e":
+    user_input = input("\033[38;5;230m[*] Enter text to Encrypt :")
+    result = encrypt_decrypt(user_input, key, mode)
+    print("\n\033[38;5;202m-----------------------------------------")
+    print("\033[38;5;39m[*] Encrypting Plain Text.... ")
+    time.sleep(1.5)
+    print(f"\033[38;5;82m[+] Encrypted Text : {result}")
+    print("\033[38;5;202m-----------------------------------------")
+elif mode == "d":
+    user_input = input("\033[38;5;230m[*] Enter text to Decrypt :")
+    result = encrypt_decrypt(user_input, key, mode)
+    print("\n\033[38;5;202m-----------------------------------------")
+    print("\033[38;5;39m[*] Decrypting Cipher.... ")
+    time.sleep(1.5)
+    print(f"\033[38;5;82m[+] Decrypted Text : {result}")
+    print("\033[38;5;202m-----------------------------------------")
